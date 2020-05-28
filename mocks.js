5b46171b5ae7a9ecdac695927954211149615c6f
Artem Ruchka <55321756+arheor@users.noreply.github.com>
2020-05-28 20:13:19 +0300

    user page

--- mocks.js
+++ mocks.js
@@ -3,7 +3,7 @@ const TurndownService = require('turndown');
 
 const models = require('./models');
 
-const owner = '5addc18c6d1e240d93be34a8';
+const owner = '5ebfdd13c9bb804170cd8c11';
 
 module.exports = () => {
   models.Post.remove()
--- routes/archive.js
+++ routes/archive.js
@@ -13,10 +13,12 @@ function posts(req, res) {
   models.Post.find({})
     .skip(perPage * page - perPage)
     .limit(perPage)
+    .populate('owner', 'login')
+    .sort({ createdAt: -1 })
     .then((posts) => {
       models.Post.count()
         .then((count) => {
-          res.render('index', {
+          res.render('archive/index', {
             user: {
               id: userId,
               login: userLogin,
@@ -74,6 +76,57 @@ router.get('/posts/:post', (req, res, next) => {
       }
     });
   }
+});
+
+// users posts
+router.get('/users/:login/:page*?', (req, res) => {
+  const userId = req.session.userId;
+  const userLogin = req.session.userLogin;
+  const perPage = +config.PER_PAGE;
+  const page = req.params.page || 1;
+  const login = req.params.login;
+
+  models.User.findOne({
+    login,
+  }).then((user) => {
+    models.Post.find({
+      owner: user.id,
+    })
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .sort({ createdAt: -1 })
+      .then((posts) => {
+        console.log(posts);
+        models.Post.count({
+          owner: user.id,
+        })
+          .then((count) => {
+            res.render('archive/user', {
+              user: {
+                id: userId,
+                login: userLogin,
+              },
+              _user: user,
+              posts: {
+                // посты найденные в базе
+                posts,
+                // текущая страница :page
+                current: page,
+                // общее кол-во страниц
+                pages: Math.ceil(count / perPage),
+                // постов на странице
+                perPage: config.PER_PAGE,
+              },
+            });
+          })
+          .catch(() => {
+            throw new Error('Server Error');
+          });
+      })
+      .catch(() => {
+        throw new Error('Server Error');
+      });
+  });
 });
 
 module.exports = router;
